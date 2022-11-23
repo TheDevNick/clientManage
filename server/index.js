a782ac5824b40975615271322161767077063199
Nick Hollins <nholl22@wgu.edu>
2022-11-23 14:28:15 -0600

    client manage first commit

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+require("dotenv").config();
+const {graphqlHTTP} = require('express-graphql')
+const schema = require('./schema/schema')
+const colors = require('colors')
+const port = process.env.PORT || 5000;
+const connectDB = require('../server/config/db')
+const app = express();
+
+// Connect to db
+connectDB()
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development'
+}))
+
+app.listen(port, console.log(`Server running on port: ${port}`.bgCyan))
